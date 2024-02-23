52ab144e926de32d8a95db64ae8046399c832243
Nils Alex <nils@famalex.de>
2024-02-23 19:45:31 +0100

    Revert "remove llm-ls"
    This reverts commit 5a82a348ed8e00ca62ba63ea5fd6e6819cad72a7.

---
+++ lua/custom/plugins/llm-ls.lua
@@ -0,0 +1,40 @@
+local api_token = os.getenv("TNG_LLM_TOKEN")
+
+return {
+  {
+    "huggingface/llm.nvim",
+    opts = {
+      api_token = api_token,
+      backend = "tgi",
+      url = "https://codepilot.model.tngtech.com/api/generate",
+      tokens_to_clear = { "<|endoftext|>" },
+      request_body = {
+        parameters = {
+          max_new_tokens = 300,
+          temperature = 0.2,
+          top_p = 0.95,
+          stop_tokens = nil,
+        },
+      },
+      fim = {
+        enabled = true,
+        prefix = "<fim_prefix>",
+        middle = "<fim_middle>",
+        suffix = "<fim_suffix>",
+      },
+      debounce_ms = 150,
+      accept_keymap = "<Tab>",
+      dismiss_keymap = "<S-Tab>",
+      tls_skip_verify_insecure = false,
+      lsp = {
+        bin_path = "/home/nils/code/huggingface/llm-ls/target/release/llm-ls",
+        version = "0.5.2",
+      },
+      tokenizer = nil,
+      context_window = 8192,
+      enable_suggestions_on_startup = false,
+      enable_suggestions_on_files = "*",
+    },
+
+  },
+}
