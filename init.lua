c7e2703e4795134cc51b91360da2027c7bc2b67a
Nils Alex <nils@famalex.de>
2024-03-06 11:07:04 +0100

    update plugins, set Jenkinsfile filetype to groovy

--- init.lua
+++ init.lua
@@ -688,3 +688,8 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.api.nvim_create_autocmd({'BufRead','BufNewFile'}, {
+  pattern = 'Jenkinsfile',
+  command = 'set filetype=groovy',
+})
