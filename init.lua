6f5e46e20a8653f99a735da0da8418a625957b30
Nils Alex <nils@famalex.de>
2024-03-24 12:58:21 +0100

    update omnisharp settings

--- init.lua
+++ init.lua
@@ -623,6 +623,11 @@ local servers = {
 
   omnisharp = {
     cmd = { "OmniSharp", "-lsp" },
+    settings = {
+      enable_roslyn_analyzers = true,
+      organize_imports_on_format = true,
+      enable_import_completion = true,
+    },
   },
 }
 
