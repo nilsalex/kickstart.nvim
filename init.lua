99b267331b9b686740472032bcb79df74e17ddf0
Nils Alex <nils@famalex.de>
2024-07-29 17:27:35 +0200

    add helm support and add lockfile

--- init.lua
+++ init.lua
@@ -635,6 +635,7 @@ require('lazy').setup({
         terraformls = {},
         tflint = {},
         pyright = {},
+        helm_ls = {},
 
         kotlin_language_server = {},
 
@@ -886,7 +887,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'gotmpl', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
