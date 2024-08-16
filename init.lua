c535221d5f602d278e567a87311c7054516fa487
Nils Alex <nils@famalex.de>
2024-08-16 16:40:04 +0200

    configure formatting for nix files

--- init.lua
+++ init.lua
@@ -700,6 +700,7 @@ require('lazy').setup({
       },
       -- You can also specify external formatters in here.
       formatters_by_ft = {
+        nix = { 'nixfmt' },
         -- rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
